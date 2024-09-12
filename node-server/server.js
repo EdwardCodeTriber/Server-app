30890fff534b31957cd78c36df6740d577267227
EdwardThapelo <edwardthapelo55@gmail.com>
2024-09-12 14:47:53 +0200

    Created a server

--- node-server/server.js
+++ node-server/server.js
@@ -4,4 +4,15 @@ const http = require('http');
 // Define the port to listen on
 const PORT = process.env.PORT || 3000;
 
+// Create a server
+const server = http.createServer((res) => {
+  // Set the response 
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  
+  // Send a response message
+  // what will be displyed
+  res.end('Hello, World!\n');
+});
+
 
