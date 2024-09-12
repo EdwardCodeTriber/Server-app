e6037c8b05d0c16c163fb27bc3d52219b9dcd76c
EdwardThapelo <edwardthapelo55@gmail.com>
2024-09-12 16:08:03 +0200

    edited the setHeader method

--- node-server/server.js
+++ node-server/server.js
@@ -1,18 +1,19 @@
 // Import the http module
-const http = require('http');
+const http = require("http");
 
 // Define the port to listen on
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Create a server
-const server = http.createServer((res) => {
-  // Set the response 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  
+const server = http.createServer((req, res) => {
+  // Set the response
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
   // Send a response message
   // what will be displyed
-  res.end('Hello, World!\n');
+  res.end("Hello, World!\n This is a server running on this port");
 });
 
 // listen on the specified port
