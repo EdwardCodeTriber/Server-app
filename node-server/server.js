5a8aaad4c730cf70d3aa63108a9fb958f531c589
EdwardThapelo <edwardthapelo55@gmail.com>
2024-09-16 13:22:58 +0200

    Added a routing request method

--- node-server/server.js
+++ node-server/server.js
@@ -18,12 +18,34 @@ const server = http.createServer((req, res) => {
   ///////////////// Task 2 Code ///////////////////////
   // Default routing on server runtime
   if (req.url === "/" && req.method === "GET") {
-      // Set the response
+    // Set the response
 
     res.writeHead(200);
-      // Send a response message
-  // what will be displyed on port http://localhost:3000/data
-    res.end(JSON.stringify({ message: "Welcome to Default Server! on runtime" }));
+    // Send a response message
+    // what will be displyed on port http://localhost:3000/data
+    res.end(
+      JSON.stringify({ message: "Welcome to Default Server! on runtime http://localhost:3000/" })
+    );
+  } else if (req.url === "/data" && req.method === "GET") {
+    // Send a response message
+    // what will be displyed
+    res.writeHead(200);
+    res.end(
+      JSON.stringify({
+        data: "Data Displayed on the http://localhost:3000/data routing.",
+      })
+    );
+  } else if (req.url === "/node" && req.method === "GET") {
+    res.writeHead(200);
+    res.end(
+      JSON.stringify({
+        data: "Written on node as a GET route data on port http://localhost:3000/node",
+      })
+    );
+  }  else {
+    // Handle invalid routes
+    res.writeHead(404);
+    res.end(JSON.stringify({ message: "Route not found or available" }));
   }
 });
 
